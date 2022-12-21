385dacf4079763e7edcbf20ff12604cff48d4f18
Jaime Sayago Heredia <jaimepsayago@gmail.com>
2022-12-21 11:59:31 -0500

    update workspace

--- poo/src/main/java/com/kruger/poo/PooApplication.java
+++ poo/src/main/java/com/kruger/poo/PooApplication.java
@@ -1,13 +1,20 @@
 package com.kruger.poo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PooApplication {
+public class PooApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(PooApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("hola");
+		
 	}
 
 }
