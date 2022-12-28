db0ae2b1e70676e0493e19db5c663c202f75bd4c
Jaime Sayago Heredia <jaimepsayago@gmail.com>
2022-12-28 10:56:46 -0500

    update crud microservice

---
+++ microservice/src/test/java/com/kruger/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruger.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
