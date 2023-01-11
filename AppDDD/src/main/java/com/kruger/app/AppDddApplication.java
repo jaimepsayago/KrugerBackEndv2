c617ea300494594a9d35745ae3feb0700081685b
Jaime Sayago Heredia <jaimepsayago@gmail.com>
2023-01-11 10:58:01 -0500

    second commit for ddd application

---
+++ AppDDD/src/main/java/com/kruger/app/AppDddApplication.java
@@ -0,0 +1,13 @@
+package com.kruger.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDddApplication.class, args);
+	}
+
+}
---
+++ AppDDD/src/test/java/com/kruger/app/AppDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruger.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
